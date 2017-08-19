ed8c7fdbbd6556699e54c302229e0c6dd54e5507
Fengfanfan <569614997@qq.com>
2017-08-19 11:22:29 -0700

    +1

--- books/models.py
+++ books/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
