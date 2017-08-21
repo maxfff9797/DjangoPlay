34299c68b62b8170fa9e29066681723e95b39aab
Fengfanfan <569614997@qq.com>
2017-08-21 14:07:06 -0700

    shit-0821

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,13 @@
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from mysite.views import hello,current_datetime,hours_ahead
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^hello/$', hello),
     url(r'^time/$',current_datetime),
-    url(r'^time/plus/(\d{1,2})/$',hours_ahead)
+    url(r'^time/plus/(\d{1,2})/$',hours_ahead),
+    url(r'^admin/',include('admin.site.urls')),
 ]
